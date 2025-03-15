2bf99ca3c17966178ccd5d37e009f8bd49b1885f
Benji <benji@gmail.com>
2025-03-15 16:08:58 +0100

    Added docString and HowTo

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymonnify",
+    version="1.0.0",
+    author="Marvelous-Benji",
+    author_email="integration-support@monnify.com",
+    description="Python library for the Monnify API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Monnify/pymonnify",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['requests', 'marshmallow'],
+    tests_require=['pytest','black'],
+    test_suite='tests',
+)
